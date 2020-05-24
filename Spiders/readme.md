2e716be9387331cf8e52285bca75df87de743480
zyt <ijumper@163.com>
2020-05-24 10:25:39 +0800

    add author spider

---
+++ Spiders/readme.md
@@ -0,0 +1,8 @@
+# 命令行
+- scrapy crawl quotes
+- scrapy shell 'http://quotes.toscrape.com/page/1/'
+- scrapy shell 'http://quotes.toscrape.com'
+- scrapy crawl quotes -o quotes.json
+- scrapy crawl quotes -o quotes.jl
+- scrapy crawl author
+- scrapy crawl quotes -o quotes-humor.json -a tag=humor 传参
--- Spiders/spiders/quotes_spider.py
+++ Spiders/spiders/quotes_spider.py
@@ -5,19 +5,25 @@
 import scrapy
 
 
+
+
 class QuotesSpider(scrapy.Spider):
     name = "quotes"
 
     def start_requests(self):
-        urls = [
-            'http://quotes.toscrape.com/page/1/',
-            'http://quotes.toscrape.com/page/2/',
-        ]
-        for url in urls:
-            yield scrapy.Request(url=url, callback=self.parse)
+        url = 'http://quotes.toscrape.com/'
+        tag = getattr(self, 'tag', None)
+        if tag is not None:
+            url = url + 'tag/' + tag
+        yield scrapy.Request(url, self.parse)
 
     def parse(self, response):
-        page = response.url.split("/")[-2]
-        filename = 'quotes-%s.html' % page
-        with open(filename, 'wb') as f:
-            f.write(response.body)
+        for quote in response.css('div.quote'):
+            yield {
+                'text': quote.css('span.text::text').get(),
+                'author': quote.css('small.author::text').get(),
+            }
+
+        next_page = response.css('li.next a::attr(href)').get()
+        if next_page is not None:
+            yield response.follow(next_page, self.parse)
